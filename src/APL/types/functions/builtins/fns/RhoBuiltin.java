0da5d76ea80c8bfe2a9b93fc20ae343af3e803b7
dzaima <atdzim@inbox.lv>
2018-10-05 18:23:36 +0300

    empty dimensions for ⍴

--- src/APL/types/functions/builtins/fns/RhoBuiltin.java
+++ src/APL/types/functions/builtins/fns/RhoBuiltin.java
@@ -1,6 +1,6 @@
 package APL.types.functions.builtins.fns;
 
-import APL.errors.DomainError;
+import APL.errors.*;
 import APL.types.*;
 import APL.types.functions.Builtin;
 
@@ -25,12 +25,25 @@ public class RhoBuiltin extends Builtin {
     if (a.rank > 1) throw new DomainError("multidimensional shape", this, a);
     int[] sh = new int[a.arr.length];
     int ia = 1;
+    Integer emptyPos = null;
     for (int i = 0; i < sh.length; i++) {
       Value v = a.arr[i];
-      if (! (v instanceof Num)) throw new DomainError("shape for ⍴ contained "+human(v.valtype, true), this, v);
-      int c = ((Num) v).intValue();
-      sh[i] = c;
-      ia*= c;
+      if (! (v instanceof Num)) {
+        if (v.ia == 0) {
+          if (emptyPos == null) emptyPos = i;
+          else throw new DomainError("shape contained multiple undefined dimension sizes", this, v);
+        } else throw new DomainError("shape for ⍴ contained "+human(v.valtype, true), this, v);
+      } else {
+        int c = ((Num) v).intValue();
+        sh[i] = c;
+        ia *= c;
+      }
+    }
+    if (emptyPos != null) {
+//      System.out.println(w.ia+" "+ia);
+      if (w.ia % ia != 0) throw new LengthError("empty dimension not perfect", this, w);
+      sh[emptyPos] = w.ia/ia;
+      ia = w.ia;
     }
     Value[] arr = new Value[ia];
     if (w.ia == 0) {
