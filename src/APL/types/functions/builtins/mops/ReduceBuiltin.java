9ae0bc910dab62ead2ed5f0142106b3b46128e23
dzaima <atdzim@inbox.lv>
2018-12-24 14:08:31 +0200

    A f/ B

--- src/APL/types/functions/builtins/mops/ReduceBuiltin.java
+++ src/APL/types/functions/builtins/mops/ReduceBuiltin.java
@@ -1,7 +1,8 @@
 package APL.types.functions.builtins.mops;
 
-import APL.errors.DomainError;
+import APL.errors.*;
 import APL.types.*;
+import APL.types.arrs.HArr;
 import APL.types.dimensions.DimMMop;
 import APL.types.functions.Mop;
 
@@ -16,7 +17,6 @@ public class ReduceBuiltin extends Mop implements DimMMop {
   }
   
   public Obj call(Obj f, Value w) {
-    // TODO ranks
     if (w.rank >= 2) {
       return ngnReduce(w, -1, (Fun) f);
     }
@@ -31,6 +31,34 @@ public class ReduceBuiltin extends Mop implements DimMMop {
     }
     return last.squeeze();
   }
+  
+  public Obj call(Obj f, Value a, Value w) {
+    if (w.rank != 1) throw new NYIError("A f/ B with 2≤⍴⍴B hasn't been implemented", w);
+    if (!(f instanceof Fun)) throw new DomainError("operand to / must be a function");
+    int n = a.asInt();
+    Value[] ra = new Value[w.ia - Math.abs(n) + 1];
+    Value[] wa = w.values();
+    if (n > 0) {
+      for (int i = 0; i < ra.length; i++) {
+        Value r = wa[i+n-1];
+        for (int j = n-2; j >= 0; j--) {
+          r = (Value) ((Fun) f).call(wa[i + j], r);
+        }
+        ra[i] = r;
+      }
+    } else {
+      n = -n;
+      for (int i = 0; i < ra.length; i++) {
+        Value r = wa[i];
+        for (int j = 1; j < n; j++) {
+          r = (Value) ((Fun) f).call(wa[i + j], r);
+        }
+        ra[i] = r;
+      }
+    }
+    return HArr.create(ra, new int[]{ra.length});
+  }
+  
   private Value ngnReduce(Value x, int axis, Fun f) { // https://chat.stackexchange.com/transcript/message/47158587#47158587
     if (x.rank == 0) return x;
     if (axis < 0) axis+= x.rank;
