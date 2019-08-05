495b3476a839908b6489adbf4d1d227309c6ec5a
dzaima <atdzim@inbox.lv>
2019-08-05 18:44:56 +0300

    n f\

--- src/APL/types/functions/builtins/mops/ScanBuiltin.java
+++ src/APL/types/functions/builtins/mops/ScanBuiltin.java
@@ -1,7 +1,8 @@
 package APL.types.functions.builtins.mops;
 
-import APL.errors.SyntaxError;
+import APL.errors.*;
 import APL.types.*;
+import APL.types.arrs.*;
 import APL.types.functions.*;
 
 public class ScanBuiltin extends Mop {
@@ -24,7 +25,31 @@ public class ScanBuiltin extends Mop {
     return Arr.create(res);
   }
   
-//  public Obj call(Value a, Value w) {
-//
-//  }
+  public Obj call(Obj aa, Value a, Value w, DerivedMop derv) {
+    int n = a.asInt();
+    int len = w.ia;
+    DomainError.must(n>=0, "⍺ of \\ should be non-negative (was "+n+")");
+    RankError.must(w.rank <= 1, "rank of ⍵ of \\ should be less than 2 (was "+w.rank+")");
+    
+    if (w.quickDoubleArr()) {
+      Value[] res = new Value[len-n+1];
+      double[] wa = w.asDoubleArr();
+      for (int i = 0; i < res.length; i++) {
+        double[] curr = new double[n];
+        System.arraycopy(wa, i, curr, 0, n);
+        res[i] = (Value) ((Fun) aa).call(new DoubleArr(curr));
+      }
+      return Arr.create(res);
+    }
+    
+    Value[] res = new Value[len-n+1];
+    Value[] wa = w.values();
+    for (int i = 0; i < res.length; i++) {
+      Value[] curr = new Value[n];
+      // for (int j = 0; j < n; j++) curr[j] = wa[i + j];
+      System.arraycopy(wa, i, curr, 0, n);
+      res[i] = (Value) ((Fun) aa).call(HArr.create(curr));
+    }
+    return Arr.create(res);
+  }
 }
