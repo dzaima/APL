ae0929c0f37144e7fffe7983f0d3462fa32257f7
dzaima <atdzim@inbox.lv>
2019-12-02 22:12:15 +0200

    ⍎`{⍹}

--- src/APL/types/functions/builtins/fns/EvalBuiltin.java
+++ src/APL/types/functions/builtins/fns/EvalBuiltin.java
@@ -2,8 +2,7 @@ package APL.types.functions.builtins.fns;
 
 import APL.*;
 import APL.errors.DomainError;
-import APL.types.Obj;
-import APL.types.Value;
+import APL.types.*;
 import APL.types.functions.*;
 
 public class EvalBuiltin extends Builtin {
@@ -16,11 +15,14 @@ public class EvalBuiltin extends Builtin {
   }
   
   public Obj call(Value w) {
+    if (w instanceof ArrFun) return ((ArrFun) w).obj();
     return Main.exec(w.asString(), sc);
   }
   public Obj call(Value a, Value w) {
     if (a instanceof ArrFun) {
-      return ((ArrFun) a).fun().call(w);
+      Obj obj = ((ArrFun) a).obj();
+      if (!(obj instanceof Fun)) throw new DomainError("⍺ of ⍎ must be `fun, was "+obj.humanType(true));
+      return ((Fun) obj).call(w);
     } else {
       throw new DomainError("Expected ⍺ of ⍎ to be an arrayified function", this, a);
     }
