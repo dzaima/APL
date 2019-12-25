5594e8056b824afc31f37888b00090a8b977ab12
dzaima <atdzim@inbox.lv>
2019-12-25 15:29:24 +0200

    f⍣¯1⍣¯1

--- src/APL/types/functions/builtins/dops/RepeatBuiltin.java
+++ src/APL/types/functions/builtins/dops/RepeatBuiltin.java
@@ -1,6 +1,7 @@
 package APL.types.functions.builtins.dops;
 
 import APL.*;
+import APL.errors.DomainError;
 import APL.types.*;
 import APL.types.functions.*;
 
@@ -28,14 +29,30 @@ public class RepeatBuiltin extends Dop {
       int am = ((Num) ww).asInt();
       if (am < 0) {
         for (int i = 0; i < -am; i++) {
-          w = (Value) ((Fun) aa).callInv(w);
+          w = (Value) f.callInv(w);
         }
       } else for (int i = 0; i < am; i++) {
-        w = (Value) ((Fun) aa).call(w);
+        w = (Value) f.call(w);
       }
       return w;
     }
   }
+  
+  public Obj callInv(Obj aa, Obj ww, Value w) {
+    Fun f = (Fun) aa;
+    if (ww instanceof Fun) throw new DomainError("(f⍣g)A cannot be inverted", this);
+    
+    int am = ((Num) ww).asInt();
+    if (am < 0) {
+      for (int i = 0; i < -am; i++) {
+        w = (Value) f.call(w);
+      }
+    } else for (int i = 0; i < am; i++) {
+      w = (Value) f.callInv(w);
+    }
+    return w;
+  }
+  
   public Obj call(Obj aa, Obj ww, Value a, Value w, DerivedDop derv) {
     int am = ((Num)ww).asInt();
     if (am < 0) {
@@ -44,6 +61,18 @@ public class RepeatBuiltin extends Dop {
       }
     } else for (int i = 0; i < am; i++) {
       w = (Value)((Fun)aa).call(a, w);
+    }
+    return w;
+  }
+  
+  public Obj callInvW(Obj aa, Obj ww, Value a, Value w) {
+    int am = ((Num)ww).asInt();
+    if (am < 0) {
+      for (int i = 0; i < -am; i++) {
+        w = (Value)((Fun)aa).call(a, w);
+      }
+    } else for (int i = 0; i < am; i++) {
+      w = (Value)((Fun)aa).callInvW(a, w);
     }
     return w;
   }
