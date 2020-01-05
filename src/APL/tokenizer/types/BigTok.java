ceea05e25687988ed0980a4abf4b9249b736543f
dzaima <atdzim@inbox.lv>
2020-01-05 17:26:25 +0200

    ⍕`{123L}

--- src/APL/tokenizer/types/BigTok.java
+++ src/APL/tokenizer/types/BigTok.java
@@ -5,11 +5,11 @@ import APL.types.BigValue;
 
 public class BigTok extends Token {
   public final BigValue val;
-  public BigTok(String raw, int spos, int epos, BigValue val) {
-    super(raw, spos, epos);
+  public BigTok(String line, int spos, int epos, BigValue val) {
+    super(line, spos, epos);
     this.val = val;
   }
   @Override public String toRepr() {
-    return null;
+    return source();
   }
 }
