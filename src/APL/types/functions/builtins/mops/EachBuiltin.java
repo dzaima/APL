c7070e2ae5d5f48ca8891c95e83ec6228a961196
dzaima <atdzim@inbox.lv>
2020-02-20 14:29:35 +0200

    fix -⍣¯1⊢1, shapes for mismatched ¨

--- src/APL/types/functions/builtins/mops/EachBuiltin.java
+++ src/APL/types/functions/builtins/mops/EachBuiltin.java
@@ -1,5 +1,6 @@
 package APL.types.functions.builtins.mops;
 
+import APL.Main;
 import APL.errors.*;
 import APL.types.*;
 import APL.types.arrs.SingleItemArr;
@@ -42,7 +43,7 @@ public class EachBuiltin extends Mop {
       }
       return Arr.create(n, w.shape);
     }
-    if (!Arrays.equals(a.shape, w.shape)) throw new LengthError("shapes not equal");
+    if (!Arrays.equals(a.shape, w.shape)) throw new LengthError("shapes not equal ("+ Main.formatAPL(a.shape)+" vs "+Main.formatAPL(w.shape)+")");
     Value[] n = new Value[w.ia];
     for (int i = 0; i < n.length; i++) {
       n[i] = ((Value)((Fun)f).call(a.get(i), w.get(i))).squeeze();
@@ -56,6 +57,7 @@ public class EachBuiltin extends Mop {
     for (int i = 0; i < n.length; i++) {
       n[i] = ((Value) ((Fun) f).callInv(w.get(i))).squeeze();
     }
+    if (w.rank == 0 && n[0] instanceof Primitive) return n[0];
     return Arr.create(n, w.shape);
   }
 }
