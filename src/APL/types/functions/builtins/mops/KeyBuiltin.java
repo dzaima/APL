ba16d66c39031af5f8031289540ddb6c6f24eb39
dzaima <atdzim@inbox.lv>
2020-04-10 20:09:36 +0300

    A f⌸ B

--- src/APL/types/functions/builtins/mops/KeyBuiltin.java
+++ src/APL/types/functions/builtins/mops/KeyBuiltin.java
@@ -1,6 +1,6 @@
 package APL.types.functions.builtins.mops;
 
-import APL.Scope;
+import APL.*;
 import APL.errors.*;
 import APL.types.*;
 import APL.types.arrs.HArr;
@@ -55,7 +55,7 @@ public class KeyBuiltin extends Mop {
       }
       return new HArr(res);
     }
-    throw new DomainError("⌸ ⍶ not map nor function");
+    throw new DomainError("⌸: ⍶ must be a function or a map, was "+aa.humanType(true), derv, aa);
   }
   
   public Value call(Obj aa, Value a, Value w, DerivedMop derv) {
@@ -63,6 +63,34 @@ public class KeyBuiltin extends Mop {
       ((APLMap)aa).set(a, w);
       return w;
     }
-    throw new NYIError("dyadic ⌸", this); // TODO
+    if (aa instanceof Fun) {
+      Fun aaf = (Fun) aa;
+      if (!Arrays.equals(a.shape, w.shape)) {
+        if (a.rank != w.rank) throw new RankError("dyadic ⌸ expected ⍺ & ⍵ to have equal ranks ("+a.rank+" vs "+w.rank+")", derv, w);
+        throw new LengthError("dyadic ⌸ expected ⍺ & ⍵ to have equal shapes ("+Main.toAPL(a.shape)+" vs "+ Main.toAPL(w.shape)+")", derv, w);
+      }
+      HashMap<Value, ArrayList<Value>> vals = new HashMap<>();
+      ArrayList<Value> order = new ArrayList<>();
+      for (int i = 0; i < a.ia; i++) {
+        Value k = w.get(i);
+        Value v = a.get(i);
+        ArrayList<Value> curr = vals.get(k);
+        if (curr == null) {
+          ArrayList<Value> newArr = new ArrayList<>();
+          vals.put(k, newArr);
+          curr = newArr;
+          order.add(k);
+        }
+        curr.add(v);
+      }
+      Value[] res = new Value[order.size()];
+      for (int i = 0; i < order.size(); i++) {
+        Value k = order.get(i);
+        Value vs = Arr.create(vals.get(k).toArray(new Value[0])); // TODO make an ArrayList HArr?
+        res[i] = aaf.call(k, vs);
+      }
+      return Arr.create(res);
+    }
+    throw new DomainError("⌸: ⍶ must be a function or a map, was "+aa.humanType(true), derv, aa); 
   }
 }
