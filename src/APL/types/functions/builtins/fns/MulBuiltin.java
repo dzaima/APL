d55ca844dca143f248e8fb4285aa44b9fd0576a9
dzaima <atdzim@inbox.lv>
2020-05-08 01:17:00 +0300

    f⍢×

--- src/APL/types/functions/builtins/fns/MulBuiltin.java
+++ src/APL/types/functions/builtins/fns/MulBuiltin.java
@@ -1,8 +1,11 @@
 package APL.types.functions.builtins.fns;
 
+import APL.Main;
 import APL.errors.DomainError;
 import APL.types.*;
 import APL.types.functions.Builtin;
+
+import java.math.BigInteger;
 
 public class MulBuiltin extends Builtin {
   @Override public String repr() {
@@ -62,5 +65,56 @@ public class MulBuiltin extends Builtin {
   
   @Override public Value callInvA(Value a, Value w) {
     return callInvW(w, a);
+  }
+  
+  
+  private static final D_NNeN SET_SGN = new D_NNeN() {
+    public double on(double o, double n) {
+      if (n==0) return 0;
+      if (o==0) throw new DomainError("⍢×: cannot set sign of 0 to "+Num.format(n));
+      if (n== 1) return  Math.abs(o);
+      if (n==-1) return -Math.abs(o);
+      throw new DomainError("⍢×: cannot set sign to "+n);
+    }
+    public void on(double[] res, double o, double[] n) {
+      for (int i = 0; i < res.length; i++) {
+        double nc = n[i];
+        if (o==0 && nc!=0) throw new DomainError("⍢×: cannot set sign of 0 to " + Num.format(nc));
+        if (nc==0 || nc==1 || nc==-1) res[i] = Math.abs(o)*nc;
+        else throw new DomainError("⍢×: cannot set sign to " + nc);
+      }
+    }
+    public void on(double[] res, double[] o, double n) {
+      for (int i = 0; i < res.length; i++) {
+        double oc = o[i];
+        if (oc==0 && n!=0) throw new DomainError("⍢×: cannot set sign of 0 to " + Num.format(n));
+        if (n==0 || n==1 || n==-1) res[i] = Math.abs(oc)*n;
+        else throw new DomainError("⍢×: cannot set sign to " + n);
+      }
+    }
+    public void on(double[] res, double[] o, double[] n) {
+      for (int i = 0; i < res.length; i++) {
+        double oc = o[i];
+        double nc = n[i];
+        if (oc==0 && nc!=0) throw new DomainError("⍢×: cannot set sign of 0 to " + Num.format(nc));
+        if (nc==0 || nc==1 || nc==-1) res[i] = Math.abs(oc)*nc;
+        else throw new DomainError("⍢×: cannot set sign to " + nc);
+      }
+    }
+    public Value call(BigValue o, BigValue n) {
+      BigInteger oi = o.i;
+      int ni = BigValue.safeInt(n.i);
+      if (oi.signum()==0 && ni!=0) throw new DomainError("⍢×: cannot set sign of 0 to " + ni);
+      if (ni== 0) return BigValue.ZERO;
+      boolean neg = oi.signum() == -1;
+      if (ni== 1 ^ neg) return o;
+      if (ni==-1 ^ neg) return new BigValue(oi.negate());
+      else throw new DomainError("⍢×: cannot set sign to " + ni);
+    }
+  };
+  public Value under(Obj o, Value w) {
+    Main.faulty = this;
+    Value v = o instanceof Fun? ((Fun) o).call(call(w)) : (Value) o;
+    return numD(SET_SGN, w, v);
   }
 }
